40ba47da78adddfbf48192c69744871799d2e854
vladislavmaluy4308 <vlad.maluy666@gmail.com>
2025-12-28 11:53:23 +0200

    complite Hero-Section

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    root: 'src',
     build: {
       sourcemap: true,
       rollupOptions: {
